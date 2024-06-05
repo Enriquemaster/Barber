759cb4271081da8c7f559cf423e047575350cb7f
JoseEduardo26 <jose.20070049@itsmotul.edu.mx>
2024-06-05 08:39:44 -0600

    registro 2

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,6 @@
 <div class="flex flex-col md:flex-row">
     <div class="w-full md:w-3/5 bg-gray-200 ">
-        <img src="{{ asset('IMG/FondoBlack2.png') }}" alt="login" class="object-cover w-full h-full" />
+        <img src="{{ asset('IMG/FondoBlack3.png') }}" alt="login" class="object-cover w-full h-full" />
     </div>
 
     <div class="w-full md:w-2/5 bg-gray-300 ">
@@ -29,7 +29,7 @@
                         <x-label for="email" value="{{ __('CORREO') }}" />
                         <div class="flex items-center">
                             <div class="p-2 bg-amber-500 mt-1 ">
-                                <img src="{{ asset('IMG/loginIconic.png') }}" alt="User Icon" class="w-6 h-auto">
+                                <img src="{{ asset('Icons/loginIconic.png') }}" alt="User Icon" class="w-6 h-auto">
                             </div>
                             <x-input id="email" class="block mt-1 w-3/4" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
                         </div>
@@ -39,7 +39,7 @@
                         <x-label for="password" value="{{ __('CONTRASEÑA') }}" />
                         <div class="flex items-center relative">
                             <div class="p-2 bg-amber-500 mt-1">
-                                <img src="{{ asset('IMG/passIconic.png') }}" alt="Lock Icon" class="w-6 h-6">
+                                <img src="{{ asset('Icons/passIconic.png') }}" alt="Lock Icon" class="w-6 h-6">
                             </div>
                             <x-input id="password" class="block mt-1 w-3/4" type="password" name="password" required autocomplete="current-password" />
                             <div class="p-2 cursor-pointer" onclick="togglePassword()">
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,6 @@
 <div class="flex flex-col md:flex-row">
 
-    <div class="w-full md:w-2/5 bg-gray-300 ">
+    <div class="w-full md:w-2/5 bg-gray-300 order-last md:order-first">
         <x-guest-layout>
         <x-authentication-card>
             <x-slot name="logo">
@@ -11,25 +11,50 @@
 
         <form method="POST" action="{{ route('register') }}">
             @csrf
+            <div class="flex justify-center mb-4">
+                <div class="w-24 h-24 bg-[#2A2928] rounded-full flex items-center justify-center">
+                    <img src="{{ asset('Icons/Regis.png') }}" alt="User Icon" class="w-1/2 h-auto">
+                </div>
+            </div>
 
-            <div>
+            <div class="mb-4">
                 <x-label for="name" value="{{ __('Nombre') }}" />
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                <div class="flex items-center">
+                    <div class="p-3 bg-[#2A2928] mt-1 ">
+                        <img src="{{ asset('Icons/User.png') }}" alt="User Icon" class="w-6 h-auto">
+                    </div>
+                    <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                </div>
             </div>
 
             <div class="mt-4">
                 <x-label for="email" value="{{ __('Correo') }}" />
+                <div class="flex items-center">
+                    <div class="p-3 bg-[#2A2928] mt-1 ">
+                        <img src="{{ asset('Icons/Correo.png') }}" alt="User Icon" class="w-6 h-auto">
+                    </div>
                 <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+                </div>
             </div>
 
             <div class="mt-4">
                 <x-label for="password" value="{{ __('Contraseña') }}" />
+                <div class="flex items-center">
+                    <div class="p-3 bg-[#2A2928] mt-1 ">
+                        <img src="{{ asset('Icons/pass.png') }}" alt="User Icon" class="w-6 h-auto">
+                    </div>
                 <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+                </div>
             </div>
 
             <div class="mt-4">
                 <x-label for="password_confirmation" value="{{ __('Confirme su contraseña') }}" />
+                <div class="flex items-center">
+                    <div class="p-3 bg-[#2A2928] mt-1 ">
+                        <img src="{{ asset('Icons/pass.png') }}" alt="User Icon" class="w-6 h-auto">
+                    </div>
                 <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
+                </div>
             </div>
 
             <div class="mt-4">
@@ -84,7 +109,7 @@
         </x-guest-layout>
     </div>
 
-        <div class="w-full md:w-3/5 bg-gray-200 ">
+        <div class="w-full md:w-3/5 bg-gray-200 order-first md:order-last ">
             <img src="{{ asset('IMG/FondoBlack2.png') }}" alt="register" class="object-cover w-full h-full" />
         </div>
 
