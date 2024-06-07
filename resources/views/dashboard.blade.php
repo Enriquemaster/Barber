8d91cf3267cc1bcd66a9108a9b2d0d5fbc3c8ede
Enriquemaster <enriquemaster154@gmail.com>
2024-06-07 14:25:11 -0600

    modifica el dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,22 +1,12 @@
 <x-app-layout>
 
  <div class="flex items-center justify-center text-white text-5xl h-full">
-<<<<<<< HEAD
 
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-  <div class="bg-zinc-900 text-white xl:p-8 rounded-lg space-y-4 h-full">
-    <div class="flex flex-col md:flex-row items-center justify-between md:space-y-0 bg-zinc-900 rounded-lg ">
-  <div class="relative flex flex-col items-center justify-center md:items-start md:w-3/4 bg-zinc-800 rounded-lg mr-4 md:h-60">
-=======
- 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
   <div class="bg-zinc-900 text-white xl:p-8 p-4 rounded-lg space-y-4 h-full ">
     <div class="flex flex-col md:flex-row items-center justify-between md:space-y-0 bg-zinc-900 rounded-lg  ">
   <div class="relative flex flex-col items-center justify-center md:items-start md:w-3/4 bg-zinc-900 rounded-lg mr-4 md:h-60">
->>>>>>> e20806e40286e76c247dec48b231b219333e2cfa
     <video autoplay muted loop class="w-full h-full object-cover opacity-50">
       <source src="VIDEO/Fondo1.mp4" type="video/mp4">
       Tu navegador no soporta la reproducción de videos.
@@ -100,11 +90,7 @@
     </div>
   </div>-->
 </div>
-<<<<<<< HEAD
  </div>
-=======
- 
->>>>>>> e20806e40286e76c247dec48b231b219333e2cfa
 </x-app-layout>
 
 <script>
